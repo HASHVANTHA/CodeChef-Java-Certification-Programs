63fc39ddde7bb3d5320926cf09698a0681adbf36
Hashvantha <hashvantham@gmail.com>
2026-06-09 14:52:26 +0530

    Added Java certification exercises

---
+++ StringConcatenation.java
@@ -0,0 +1,6 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String s = "5";
+        System.out.println(s + 5);
+    }
+}
---
+++ StringLengthOperator.java
@@ -0,0 +1,14 @@
+public class StringLengthOperator {
+    public static void main (String[] args) 
+	{
+		String one = "Coding";
+        String two = "on";
+        String three = "CodeChef";
+        String four = "Coding on CodeChef";
+        System.out.println(one + " - " + one.length());
+        System.out.println(two + " - " + two.length());
+        System.out.println(three + " - " + three.length());
+        System.out.println(four + " - " + four.length());
+        
+	}
+}
